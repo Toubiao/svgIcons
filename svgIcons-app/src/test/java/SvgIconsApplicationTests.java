02dea9ed978c2493df84f2804c77d3057221dab8
Toubia Oussama, I221 extern (PEXT) <Oussama.Toubia@rpn.ch>
2019-11-10 14:41:17 +0100

    add configuration for spring boot test

--- svgIcons-app/src/test/java/SvgIconsApplicationTests.java
+++ svgIcons-app/src/test/java/SvgIconsApplicationTests.java
@@ -1,13 +1,17 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import xyz.hos6dev.svgIcons.svgIconsApp.logic.FilesResolver;
+import xyz.hos6dev.svgIcons.svgIconsApp.logic.FilesResolverDev;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = xyz.hos6dev.svgIcons.svgIconsApp.SvgIconsApplication.class)
 class SvgIconsApplicationTests {
 
 	@Autowired
-	private FilesResolver filesResolver;
+	private FilesResolverDev filesResolver;
 
 	@Test
 	void contextLoads() {
